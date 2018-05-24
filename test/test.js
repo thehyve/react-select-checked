c70559d2bed556cfb22ac6128e21c2522a45d5a0
Fedde Schaeffer <fedde@thehyve.nl>
2018-05-24 11:34:38 +0200

    Parametrise synchronicity to test both scenarios

--- test/test.js
+++ test/test.js
@@ -5,19 +5,37 @@ import { assert } from 'chai';
 import { mount } from 'enzyme';
 import sinon from 'sinon';
 
-describe('CheckedSelect', () => {
-    it.skip('does something', () => {
-        // given
-        const options = [{ label: 'one', value: 1 }];
-        const selection = [{ value: 1 }];
-        const changeHandler = sinon.spy();
-        // when
-        const wrapper = mount(<CheckedSelect
-            options={ options }
-            value={ selection }
-            onChange={ changeHandler }
-        />);
-        // then
-        assert.isTrue(changeHandler.notCalled);
+describe('CheckedSelect behaviour independent of synchronicity', () => {
+    const asyncOptions = [
+        {
+            asyncValue: false,
+            makeOpts: opts => opts,
+            makeLoadOpts: opts => undefined
+        },
+        {
+            asyncValue: true,
+            makeOpts: opts => undefined,
+            makeLoadOpts: opts => (() => Promise.resolve(opts))
+         },
+    ];
+    asyncOptions.forEach(({ asyncValue, makeOpts, makeLoadOpts }) => {
+        describe(`with async={ ${ asyncValue } }`, () => {
+            it.skip('does something', () => {
+                // given
+                const options = [{ label: 'one', value: 1 }];
+                const selection = [{ value: 1 }];
+                const changeHandler = sinon.spy();
+                // when
+                const wrapper = mount(<CheckedSelect
+                    async={ asyncValue }
+                    options={ makeOpts(options) }
+                    loadOptions={ makeLoadOpts(options) }
+                    value={ selection }
+                    onChange={ changeHandler }
+                />);
+                // then
+                assert.isTrue(changeHandler.notCalled);
+            });
+        });
     });
 });
